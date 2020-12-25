c318bde6157dc2e7c64a5d0aac2499785f8e17d7
artswork <haihai.lu@yahoo.com>
2020-12-25 16:33:04 +0800

    quick update content

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,13 @@ export default function About() {
   return (
     <div style={{ color: `green` }}>
       <Header headerText="About Gatsby, lu haibo Sign Up. " />
-      <p>Such wow. Very React.</p>
+      <p>Smart.</p>
+      <p>
+        习惯可以潜移默化之间重新塑造一个人，尤其是易执行的简单的细小的习惯。
+        好习惯的作用因人而已，源于我们的执行力强度有所不同，工作 10
+        年来，我整理一些自己了解的习惯分享给大家，有的习惯或许如今我也已放弃了，但过往当时都曾让我受益，之所以是
+        10 个，是为了凑整。
+      </p>
     </div>
   )
 }
