053b97ef9ce27a737801d5a0ddbcc69f20549857
artswork <haihai.lu@yahoo.com>
2020-12-25 16:49:22 +0800

    h1

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ export default function Home() {
     <div style={{ color: `purple` }}>
       <Link to="/contact/">Contact</Link>
       <Header headerText="最怕突然听到你的消息，想念如果有声音" />
+      <h1>突然好想你！</h1>
       <p>我不是一定要你回来</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
